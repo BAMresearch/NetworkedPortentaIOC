d84700f96e55592ec7e37cdb039b2f3dd4725729
toqduj <brian.pauw@bam.de>
2024-12-03 15:24:48 +0100

    t7

--- NetworkedPortentaIOC.py
+++ NetworkedPortentaIOC.py
@@ -67,54 +67,54 @@ class PortentaIOC(PVGroup):
         dtype=PvpropertyString,
     )
 
-    do0 = pvproperty(value=0, name="do0", doc="Digital output 0, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')   
-    do1 = pvproperty(value=0, name="do1", doc="Digital output 1, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
-    do2 = pvproperty(value=0, name="do2", doc="Digital output 2, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
-    do3 = pvproperty(value=0, name="do3", doc="Digital output 3, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
-    do4 = pvproperty(value=0, name="do4", doc="Digital output 4, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
-    do5 = pvproperty(value=0, name="do5", doc="Digital output 5, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
-    do6 = pvproperty(value=0, name="do6", doc="Digital output 6, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
-    do7 = pvproperty(value=0, name="do7", doc="Digital output 7, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
+    do0 = pvproperty(name="do0", doc="Digital output 0, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')   
+    do1 = pvproperty(name="do1", doc="Digital output 1, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
+    do2 = pvproperty(name="do2", doc="Digital output 2, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
+    do3 = pvproperty(name="do3", doc="Digital output 3, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
+    do4 = pvproperty(name="do4", doc="Digital output 4, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
+    do5 = pvproperty(name="do5", doc="Digital output 5, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
+    do6 = pvproperty(name="do6", doc="Digital output 6, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
+    do7 = pvproperty(name="do7", doc="Digital output 7, can be 'Low' or 'High'", enum_strings=['Low', 'High'], dtype=ChannelType.ENUM, record='bo')
     @do0.putter
     async def do0(self, instance, value):
         print(f"Setting DO0 to {value}")
         portenta_write(self.address, self.port, "DO", 0, value)
-        await instance.write(value)
+        await self.do0.write(value)
     @do1.putter
     async def do1(self, instance, value):
         print(f"Setting DO1 to {value}")
         portenta_write(self.address, self.port, "DO", 1, value)
-        await instance.write(value)
+        await self.do1.write(value)
     @do2.putter
     async def do2(self, instance, value):
         print(f"Setting DO2 to {value}")
         portenta_write(self.address, self.port, "DO", 2, value)
-        await instance.write(value)
+        await self.do2.write(value)
     @do3.putter
     async def do3(self, instance, value):
         print(f"Setting DO3 to {value}")
         portenta_write(self.address, self.port, "DO", 3, value)
-        await instance.write(value)
+        await self.do3.write(value)
     @do4.putter
     async def do4(self, instance, value):
         print(f"Setting DO4 to {value}")
         portenta_write(self.address, self.port, "DO", 4, value)
-        await instance.write(value)
+        await self.do4.write(value)
     @do5.putter
     async def do5(self, instance, value):
         print(f"Setting DO5 to {value}")
         portenta_write(self.address, self.port, "DO", 5, value)
-        await instance.write(value)
+        await self.do5.write(value)
     @do6.putter
     async def do6(self, instance, value):
         print(f"Setting DO6 to {value}")
         portenta_write(self.address, self.port, "DO", 6, value)
-        await instance.write(value)
+        await self.do6.write(value)
     @do7.putter
     async def do7(self, instance, value):
         print(f"Setting DO7 to {value}")
         portenta_write(self.address, self.port, "DO", 7, value)
-        await instance.write(value)
+        await self.do7.write(value)
 
     update_hook = pvproperty(value=0, name="update_hook", doc="Update hook for the IOC", read_only=True)
     @update_hook.scan(period=10)
